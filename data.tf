4d9ae67f0f17e7da0b6e72550e94ee6fe00f2122
playbuilder <playbuilder47@gmail.com>
2023-06-03 18:41:09 +0900

    feat(data): add ami and az data sources for both regions

---
+++ data.tf
@@ -0,0 +1,49 @@
+data "aws_availability_zones" "primary" {
+  state = "available"
+}
+
+data "aws_availability_zones" "secondary" {
+  provider = aws.seoul
+  state    = "available"
+}
+
+data "aws_ami" "va_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ami" "seoul_ami" {
+  provider    = aws.seoul
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
